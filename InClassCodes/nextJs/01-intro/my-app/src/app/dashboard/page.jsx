d09475b4b533544cd790f8086ea5e0c4e557dcd9
MustSel <mustafa.selcuk.tls@gmail.com>
2024-05-29 05:07:41 +0300

    nextjs intro

---
+++ InClassCodes/nextJs/01-intro/my-app/src/app/dashboard/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+    <div>
+        <h1 className=' text-3xl text-center capitalize'>
+            hi mannn!
+        </h1>
+    </div>
+  )
+}
+
+export default Dashboard
